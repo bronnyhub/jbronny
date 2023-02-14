7021248518d3704a1e35c9153819de2cee79149a
bronnyhub <julka.bronny@gmail.com>
2023-02-14 08:56:11 +0100

    1.2
    In progress.

--- src/components/main.js
+++ src/components/main.js
@@ -4,7 +4,10 @@ import './main.css'
 
 const Main = () => {
   return (
-    <div>main    </div>
+    <div className='main-text'>
+      <p>Hi! This page is still in progress. You should check out rest of my projects!</p>
+      <button className='github-button' onclick=" window.open('https://github.com/bronnyhub','_blank')">Click!</button>
+    </div>
   )
 }
 
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -12,11 +12,15 @@ const Sidebar = () => {
                 <li>Contacts</li>
             </ul>
 
+      <div>
         <ul className='ikonki'>
-            <li><img src='linkedin.png' className='ikona'></img></li>{/*<a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons">Linkedin icons created by riajulislam - Flaticon</a> */}
-            <li><img src='github.png' className='ikona'></img></li>{/*<a href="https://www.flaticon.com/free-icons/github" title="github icons">Github icons created by riajulislam - Flaticon</a> */}
+            <li><a href='https://www.linkedin.com/in/julia-bronny-180034262/' target='_blank' rel="noreferrer"><img src='linkedin.png' className='ikona' alt='Flaticon Linkedin'></img></a></li>
+            {/*https://www.flaticon.com/free-icons/linkedin - Linkedin icons created by riajulislam - Flaticon*/}
 
+            <li><a href='https://github.com/bronnyhub' target='_blank' rel="noreferrer"><img src='github.png' className='ikona' alt='Flaticon Github'></img></a></li>
+            {/*https://www.flaticon.com/free-icons/github - Github icons created by riajulislam - Flaticon*/}
         </ul>
+      </div>
     </div>
   )
 }
