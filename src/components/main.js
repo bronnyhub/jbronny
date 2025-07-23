9be4984c8a3e6445b7d914cd06263fe38b7f2d71
Julia Bronny <98423263+bronnyhub@users.noreply.github.com>
2025-07-23 08:25:00 +0200

    some changes

--- src/components/main.js
+++ src/components/main.js
@@ -5,7 +5,8 @@ import './main.css'
 const Main = () => {
   return (
     <div className='main-text'>
-      <p>Hi! This page is still in progress. You should check out <a href='https://github.com/bronnyhub'>rest of my projects</a>!</p>
+      <p>Hi! This page is still in progress. You should check out 
+        <a href='https://github.com/bronnyhub'>rest of my projects</a>!</p>
     </div>
   )
 }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -5,8 +5,8 @@ const Sidebar = () => {
   return (
     <div className='container-sideb'>
 
-      <img src='profile pic.png' alt='logo' className='logo'></img>
-      <img src='bronnyhub.png' alt='@bronnyhub' className='bronnyhub'></img>
+      <img src='/img/profile pic.png' alt='logo' className='logo'></img>
+      <img src='/img/bronnyhub.png' alt='@bronnyhub' className='bronnyhub'></img>
         
             <ul className='menu'>
                 <li>About me</li>
@@ -16,10 +16,10 @@ const Sidebar = () => {
 
       <div>
         <ul className='ikonki'>
-            <li><a href='https://www.linkedin.com/in/julia-bronny-180034262/' target='_blank' rel="noreferrer"><img src='linkedin.png' className='ikona' alt='Flaticon Linkedin'></img></a></li>
+            <li><a href='https://www.linkedin.com/in/julia-bronny-180034262/' target='_blank' rel="noreferrer"><img src='/img/linkedin.png' className='ikona' alt='Flaticon Linkedin'></img></a></li>
             {/*https://www.flaticon.com/free-icons/linkedin - Linkedin icons created by riajulislam - Flaticon*/}
 
-            <li><a href='https://github.com/bronnyhub' target='_blank' rel="noreferrer"><img src='github.png' className='ikona' alt='Flaticon Github'></img></a></li>
+            <li><a href='https://github.com/bronnyhub' target='_blank' rel="noreferrer"><img src='github.png' className='/img/ikona' alt='Flaticon Github'></img></a></li>
             {/*https://www.flaticon.com/free-icons/github - Github icons created by riajulislam - Flaticon*/}
         </ul>
       </div>
